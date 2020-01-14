e3eb1cd3e28f07618c7a35ce4c8425e0731b7b98
Lukas Jakobsen <109243@RTS-365.dk>
2020-01-14 14:58:46 +0100

    chat box + start pwa

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ server.listen(process.env.PORT || 3000, handlerServer);
 
 function handleSocketConnection(socket) {
 	console.log("A user connected");
+	socket.on('disconnect', handleSocketDisconnnect)
+	socket.on('message', function handleChatMessage(msg){
+		socket.broadcast.emit("message", msg);
+	})
+	socket.on("userjoin", function handleUserJoin(msg){
+		socket.broadcast.emit("userjoin", msg);
+	})
 }
+
+function handleSocketDisconnnect(){
+	console.log('user disconnected');
+}
+
 
 function handlerServer() {
 	console.log(`Listening on *:${server.address().port}`);
